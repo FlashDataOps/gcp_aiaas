8a591d7b9358e56970a0e43b50a65420490a2ae0
pablo moreno <pablomoreno@pablos-MacBook-Pro.local>
2024-09-16 09:18:44 +0200

    Hello world for alvaro

--- main.tf
+++ main.tf
@@ -4,29 +4,6 @@ provider "google" {
   region      = var.region
 }
 
-# resource "google_cloud_run_job" "default" {
-#   name     = "hello-world-job"
-#   location = var.region
-
-#   template {
-#     spec {
-#       containers {
-#         image = "gcr.io/${var.project_id}/hello-world:latest"
-#         resources {
-#           limits = {
-#             memory = "512Mi"
-#             cpu    = "1"
-#           }
-#         }
-#         # command = ["python"]
-#         # args    = ["HelloWorld.py"]
-#       }
-#     }
-#   }
-
-  # Optionally, configure retry settings and other job parameters here
-# }
-
 resource "google_cloud_run_v2_job" "default" {
   name     = "hello-world-job"
   location = var.region
@@ -40,12 +17,6 @@ resource "google_cloud_run_v2_job" "default" {
     }
   }
 }
-
-# resource "google_project_iam_member" "run_invoker" {
-#   project = var.project_id
-#   role    = "roles/run.invoker"
-#   member  = "user:allAuthenticatedUsers"
-# }
 
 output "cloud_run_job_name" {
   value = google_cloud_run_v2_job.default.name
