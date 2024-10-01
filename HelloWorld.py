a155cffefc24c477a57e888427e32fdd9c366ff0
pablo moreno <pablomoreno@pablos-MacBook-Pro.local>
2024-10-01 18:52:12 +0200

    Adding vision model capabilities and attach files in app

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,7 @@
+import io
 import langchain
 import vertexai
+from vertexai.generative_models import GenerativeModel, Part
 import requests
 from google.cloud import storage
 
@@ -9,17 +11,21 @@ from langchain.chains import (
 
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_google_vertexai import VertexAI, VertexAIEmbeddings
+from langchain_google_vertexai import VertexAI, VertexAIEmbeddings, ChatVertexAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 storage_client = storage.Client()
+bucket_name = 'single-cirrus-435319-f1-bucket'
+bucket = storage_client.bucket(bucket_name)
 
 def main(query: str)->str:
 
     # Load GOOG's 10K annual report (92 pages).
     url = "https://abc.xyz/assets/investor/static/pdf/20230203_alphabet_10K.pdf"
+
     # Set up your GCS bucket name and destination file path
-    bucket_name = 'single-cirrus-435319-f1-bucket'
     destination_blob_name = 'test_data/20230203_alphabet_10K.pdf'
 
     # Download the PDF
@@ -28,19 +34,18 @@ def main(query: str)->str:
 
     # Create a temporary file to store the downloaded PDF
     temp_file_path = '/tmp/temp_pdf.pdf'
+
     with open(temp_file_path, 'wb') as temp_file:
         temp_file.write(response.content)
 
-    bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-
     blob.upload_from_filename(temp_file_path)
     
     loader = PyPDFLoader(temp_file_path)
     documents = loader.load()
 
     # split the documents into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150, separators=["\n\n", "\n", ".", " "])
     docs = text_splitter.split_documents(documents)
 
     vertexai.init(project="single-cirrus-435319-f1")
@@ -60,7 +65,7 @@ def main(query: str)->str:
     db = Chroma.from_documents(docs, embeddings)
 
     # Expose index to the retriever
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 2})
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
     # Create chain to answer questions
 
@@ -73,11 +78,70 @@ def main(query: str)->str:
     result = qa({"query": query})
     return result['result']
 
+def main_vision(query: str, uploaded_file: io.BytesIO)->str:
+
+    vertexai.init(project="single-cirrus-435319-f1")
+
+    blob = bucket.blob(f'test_data/{uploaded_file.name}')
+
+    # Create a temporary file to store the downloaded PDF
+    temp_file_path = '/tmp/temp_pdf.pdf'
+
+    # Write the contents of the uploaded file (BytesIO) to the temporary file
+    with open(temp_file_path, 'wb') as temp_file:
+        temp_file.write(uploaded_file.read())  # Read the BytesIO content and write to the file
+
+    # Rewind the BytesIO object after reading
+    uploaded_file.seek(0)
+
+    blob.upload_from_filename(temp_file_path)
+
+    # Upload the file from BytesIO
+    blob.upload_from_file(uploaded_file, rewind=True, content_type='application/pdf')
+
+    imagen = fr"gs://{bucket_name}/test_data/{uploaded_file.name}"
+    # imagen = fr"gs://single-cirrus-435319-f1-bucket/test_data/foto01.jpg"
+
+    llm = ChatVertexAI(
+    model_name="gemini-1.5-flash-002",
+    project="single-cirrus-435319-f1",
+    verbose=True)
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Describe the image provided",
+            ),
+            
+            ("human", "{input}"),
+            (
+                "human",
+                [
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": "{image_data}"},
+                    }
+                ],
+            ),
+        ]
+    )
+
+    chain = prompt | llm | StrOutputParser()
+
+    for chunk in chain.stream({"input": query,"image_data": imagen}):
+        yield chunk
+
 if __name__ == '__main__':
     import warnings
     warnings.filterwarnings("ignore")
     print(f"LangChain version: {langchain.__version__}")
     print(f"vertexai version: {vertexai.__version__}")
-    query = "What is the State or other jurisdiction of incorporation or organization of Alphabet Inc.?"
-    response = main(query)
+    # Mocked PDF file data (minimal PDF structure in bytes)
+    mock_pdf_content = b'%PDF-1.4\n%Mock PDF file content\n%%EOF'
+    # Create a mock BytesIO object to simulate file upload
+    mock_uploaded_file = io.BytesIO(mock_pdf_content)
+    mock_uploaded_file.name = 'mock_file.pdf'  # Simulate the file name
+    query = "Summarize the pdf content"
+    response = main_vision(query, mock_uploaded_file)
     print(response)
--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from HelloWorld import main
+from HelloWorld import main, main_vision
 
 # Define a function to process the user input
 def get_chatbot_response(user_input):
     # For demonstration purposes, we return a simple response
     # You can replace this with actual chatbot logic or a call to a machine learning model
     if user_input:
-        return main(query=user_input)
+        for file in uploaded_files:
+            return main_vision(query=user_input,uploaded_file=file)
     else:
         return "Please enter a valid input."
 
@@ -18,11 +19,14 @@ st.write("Type something to chat with Monty:")
 # Text input box for user input
 user_input = st.text_input("You:", "")
 
+# File uploader to allow users to attach files or images
+uploaded_files = st.file_uploader("Attach files or images (optional):", accept_multiple_files=True)
+
 # Display chatbot response when input is provided
 if st.button("Send"):
     if user_input:
         response = get_chatbot_response(user_input)
-        st.write(f"Bot: {response}")
+        st.write_stream(response)
     else:
         st.write("You haven't typed anything!")
 
